53ac10840249b313f2f3934370533b3128a49803
NingetsuSama <hamzavaga@yahoo.fr>
2024-05-16 17:17:09 +0200

    nettoyage

--- src/backend/TogetherAgainstCancer/routers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers
-
-from tac.viewsets import CancerViewSet
-
-router = routers.SimpleRouter()
-
-router.register("tac", CancerViewSet, basename="tac")
-
-urlpatterns = router.urls
