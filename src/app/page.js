065cb9e074a9eccf31009dbe86dc5b1ab96041c8
NingetsuSama <hamzavaga@yahoo.fr>
2024-05-18 16:42:47 +0200

    ahhhhhhhhhhh

--- src/app/page.js
+++ src/app/page.js
@@ -40,16 +40,16 @@ function Carousel({ currentCancer }) {
     <div id="carouselGraphs" className="carousel carousel-fade slide">
       <div className="carousel-inner">
         <div className="carousel-item ">
-          <img src={isProd ? "/TogetherAgainstCancer" : "" + "/graphs/" + (currentCancer.type).toLowerCase() + "/daly.png"} className="d-block w-100" />
+          <img src={(isProd ? "/TogetherAgainstCancer" : "") + "/graphs/" + (currentCancer.type).toLowerCase() + "/daly.png"} className="d-block w-100" />
         </div>
         <div className="carousel-item active">
-          <img src={isProd ? "/TogetherAgainstCancer" : "" + "/graphs/" + (currentCancer.type).toLowerCase() + "/hbar.png"} className="d-block w-100" />
+          <img src={(isProd ? "/TogetherAgainstCancer" : "") + "/graphs/" + (currentCancer.type).toLowerCase() + "/hbar.png"} className="d-block w-100" />
         </div>
         <div className="carousel-item active">
-          <img src={isProd ? "/TogetherAgainstCancer" : "" + "/graphs/" + (currentCancer.type).toLowerCase() + "/time_series.png"} className="d-block w-100" />
+          <img src={(isProd ? "/TogetherAgainstCancer" : "") + "/graphs/" + (currentCancer.type).toLowerCase() + "/time_series.png"} className="d-block w-100" />
         </div>
         <div className="carousel-item active">
-          <img src={isProd ? "/TogetherAgainstCancer" : "" + "/graphs/" + (currentCancer.type).toLowerCase() + "/vbar.png"} className="d-block w-100" />
+          <img src={(isProd ? "/TogetherAgainstCancer" : "") + "/graphs/" + (currentCancer.type).toLowerCase() + "/vbar.png"} className="d-block w-100" />
         </div>
       </div>
       <button className="carousel-control-prev" type="button" data-bs-target="#carouselGraphs" data-bs-slide="prev">
